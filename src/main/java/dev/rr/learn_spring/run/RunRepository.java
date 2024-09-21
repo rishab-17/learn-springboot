ca44dbdda51be3574faccec38af4e3c55837e4d7
rishab-17 <rishab2002ramesh@gmail.com>
2024-09-21 23:18:23 +0530

    Added Runs

---
+++ src/main/java/dev/rr/learn_spring/run/RunRepository.java
@@ -0,0 +1,24 @@
+package dev.rr.learn_spring.run;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>(); //Type Run record. A sortof data storage to hold all the runs.
+
+    List<Run> findAll(){
+        return runs;
+    }
+
+    @PostConstruct // Does initialisation. Method that needs to be executed after DI is done.
+
+    private void init(){
+        //code
+        runs.add(new Run(1, "Nice Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR));
+        runs.add(new Run(2, "Super Run", LocalDateTime.now(), LocalDateTime.now().plus(2, ChronoUnit.HOURS), 3, Location.OUTDOOR));
+
+    }
+}
