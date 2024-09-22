1edc1c9acfda37052a175b0279ee1f3731268d82
rishab-17 <rishab2002ramesh@gmail.com>
2024-09-22 12:27:45 +0530

    Get mapping by ID

--- src/main/java/dev/rr/learn_spring/run/RunController.java
+++ src/main/java/dev/rr/learn_spring/run/RunController.java
@@ -1,14 +1,60 @@
 package dev.rr.learn_spring.run;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+
+//Controllers usually do not have a lot of logic. Just need to take in a request and return a response.
 
 @RestController
+@RequestMapping("/api/runs") //Rather than having individual lines of code for each of get, post etc, use requestmapping
+
 public class RunController {
 
-    @GetMapping("/hello")
-    String printHello(){
-        return "This is hello";
+    private final RunRepository runRepository;
+
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
     }
 
+
+    
+    //@GetMapping("/api/runs")
+    @GetMapping("")
+    List <Run> findAllFromRepo(){
+        return runRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    Run findIdFromRepo(@PathVariable Integer id){
+        Optional <Run> run = runRepository.findById(id);
+
+        if(run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return run.get();
+    }
+
+    //post
+    @PostMapping
+    void createRunFromRepo(@RequestBody Run run){
+        runRepository.createRun(run);
+
+    }
+    
+    //put
+
+
+    //delete
 }
--- src/main/java/dev/rr/learn_spring/run/RunRepository.java
+++ src/main/java/dev/rr/learn_spring/run/RunRepository.java
@@ -1,16 +1,27 @@
 package dev.rr.learn_spring.run;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Optional;
 
+
+@Repository
 public class RunRepository {
     private List<Run> runs = new ArrayList<>(); //Type Run record. A sortof data storage to hold all the runs.
 
     List<Run> findAll(){
         return runs;
+    }
+
+    Optional<Run> findById(Integer id){
+
+        return runs.stream().filter(run -> run.id() == id).findFirst();
+
     }
 
     @PostConstruct // Does initialisation. Method that needs to be executed after DI is done.
